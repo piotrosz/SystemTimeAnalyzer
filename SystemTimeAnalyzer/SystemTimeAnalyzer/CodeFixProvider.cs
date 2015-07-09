d1ba42b9a7fdd6ff40a8c557a84afc5c0f67f664
Piotrosz <piotrosz@gmail.com>
2015-07-10 01:13:47 +0200

    Initial commit

---
+++ SystemTimeAnalyzer/SystemTimeAnalyzer/CodeFixProvider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SystemTimeAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SystemTimeAnalyzerCodeFixProvider)), Shared]
+    public class SystemTimeAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(SystemTimeAnalyzerAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+        
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create("Replace with SystemTime.Now()", 
+                    cancellationToken => ConvertToSystemTimeNowAsync(context.Document, diagnostic, cancellationToken)),
+                diagnostic);
+
+            return Task.FromResult(0);
+        }
+
+        private async static Task<Document> ConvertToSystemTimeNowAsync(
+            Document document,
+            Diagnostic diagnostic,
+            CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            
+            var memberAccessExpressionSyntax = root.FindNode(diagnostic.Location.SourceSpan)
+                .DescendantNodesAndSelf()
+                .OfType<MemberAccessExpressionSyntax>()
+                .First();
+
+            var newRoot = root.ReplaceNode(memberAccessExpressionSyntax,
+                SyntaxFactory.ParseExpression("SystemTime.Now()")
+                .WithLeadingTrivia(memberAccessExpressionSyntax.GetLeadingTrivia())
+                .WithTrailingTrivia(memberAccessExpressionSyntax.GetTrailingTrivia()));
+
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return newDocument;
+        }
+    }
+}
---
+++ SystemTimeAnalyzer/SystemTimeAnalyzer/DiagnosticAnalyzer.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SystemTimeAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SystemTimeAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SystemTimeAnalyzer";
+
+        internal static readonly LocalizableString Title = "Use SystemTime.Now()";
+        internal static readonly LocalizableString MessageFormat = "Use SystemTime.Now() instead of DateTime.Now";
+        internal static readonly LocalizableString Description = "Do not use DateTime.Now use SystemTime.Now()";
+        internal const string Category = "Usage";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            Title, 
+            MessageFormat, 
+            Category, 
+            DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true, 
+            description: Description,
+            helpLinkUri: "http://ayende.com/blog/3408/dealing-with-time-in-tests");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            // TODO: check if is not auto-generated
+            // see: https://github.com/code-cracker/code-cracker/blob/master/src/Common/CodeCracker.Common/Extensions/GeneratedCodeAnalysisExtensions.cs
+
+            var memberAccessExpression = (MemberAccessExpressionSyntax)context.Node;
+
+            if (memberAccessExpression?.Name.ToString() != "Now" ||
+                memberAccessExpression.Expression.ToString() != "DateTime")
+            {
+                return;
+            }
+            
+            var diagnostic = Diagnostic.Create(
+                Rule, 
+                memberAccessExpression.GetLocation());
+
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
